a4bc24f256e42d154443c595a778fc308caca0b6
Nick Caravias Pi <nick.caravias@gmail.com>
2022-09-09 14:01:56 -0400

    png working

--- cam-png.py
+++ cam-png.py
@@ -1,10 +1,12 @@
 from picamera import PiCamera
 from time import sleep
+file_name = input('Enter file name: ' )
+file_path = '/home/ncaravias/label-printer/' + file_name + '.jpg'
 
 camera = PiCamera()
 
 camera.start_preview()
 sleep(5)
-camera.capture('/home/ncaravias/testAPNG.png')
+camera.capture(file_path)
 camera.stop_preview()
 
